cbb16e35c04aa04a158cb65ec5c4baaa4f119d3d
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-03-17 13:58:22 +0100

    .

---
+++ app/app.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import Hi from "./components/Hi"
+
+ReactDOM.render(<Hi />, document.getElementById('app'))
---
+++ app/components/Hi.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default class Hello extends React.Component {
+    render() {
+        return (
+            <h3>Hi!</h3>
+        )
+    }
+}
